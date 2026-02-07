a4ed66d47fee194f540a95a8ee175bfe4d9b588a
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-07 16:08:12 -0600

    :sparkles: Register providers on users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UserRepository } from './users.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [UsersService],
-  controllers: [UsersController]
+  providers: [UsersService, UserRepository, PrismaService],
+  controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
