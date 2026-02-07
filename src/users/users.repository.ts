4717c0c8c570f0462b782fc5917cccc2d71aad5f
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-07 16:05:53 -0600

    :sparkles: Implement users repository and its methods

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service.js';
+import { Prisma } from '../generated/prisma/client.js';
 
 @Injectable()
-export class UserRepository {}
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  getByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+}
