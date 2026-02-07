c1795ced3ec7fe5e0e2b9d53a14b9e65e836fe1b
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-07 16:07:26 -0600

    :sparkles: Implement users service and its methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { User } from '../generated/prisma/client.js';
+import { UserRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto.js';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async createUser(data: CreateUserDto): Promise<User | null> {
+    const userExists = (await this.userRepository.getByEmail(data.email))
+      ? true
+      : false;
+
+    if (userExists) throw new ConflictException('Email already in registered');
+
+    return this.userRepository.create(data);
+  }
+}
