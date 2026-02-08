ef9263ba2f2a3c80ae5f8351e3086a62fb934a26
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-07 19:10:17 -0600

    :sparkles: Add login and registration functionality
    Implement the login and registration endpoints for authentication. This
    includes hashing passwords, comparing credentials, and generating JWT
    tokens.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,55 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UsersService } from '../users/users.service.js';
+import { CryptoService } from '../crypto/crypto.service.js';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto.js';
+import { AuthJwtPayload } from './types/auth-jwt.payload.js';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly cryptoService: CryptoService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(registerUserData: RegisterDto) {
+    const hashedPassword = await this.cryptoService.hashPasword(
+      registerUserData.password,
+    );
+
+    const user = await this.usersService.createUser({
+      ...registerUserData,
+      password: hashedPassword,
+    });
+
+    return { user };
+  }
+
+  async login(loginData: LoginDto) {
+    const user = await this.usersService.findByEmail(loginData.email);
+    if (!user)
+      throw new BadRequestException(
+        `User with email ${loginData.email} not found`,
+      );
+
+    const passwordMatches = await this.cryptoService.comparePassword(
+      loginData.password,
+      user.password,
+    );
+    if (!passwordMatches) throw new UnauthorizedException('Invalid password');
+
+    const payload: AuthJwtPayload = {
+      sub: user.id,
+      email: user.email,
+    };
+    const token = await this.jwtService.signAsync(payload);
+
+    return { token };
+  }
 }
