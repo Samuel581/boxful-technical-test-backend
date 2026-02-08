e6ba9617057e339cfbd3de81dfb1fb8981417baf
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-07 22:27:13 -0600

    :wrench: Update auth module config to include JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,18 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { CryptoModule } from 'src/crypto/crypto.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UsersModule, CryptoModule, JwtModule],
+  imports: [
+    UsersModule,
+    CryptoModule,
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
