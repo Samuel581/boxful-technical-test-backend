9bf35055e2dff63b452b31ceac64ea7559253c8d
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-09 17:49:47 -0600

    :safety_vest: Add NotFoundException to order service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,28 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { OrdersRepository } from './orders.repository';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { Prisma } from '../generated/prisma/client';
 import { Order } from '../generated/prisma/client';
 @Injectable()
 export class OrdersService {
-    constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(private readonly ordersRepository: OrdersRepository) {}
 
-    async createOrder(createOrderDto: CreateOrderDto, userId: string): Promise<Order | null> {
-        const { packages, ...orderData } = createOrderDto;
-        const order = await this.ordersRepository.createOrder({
-            ...orderData,
-            programedDate: new Date(orderData.programedDate),
-            user: { connect: { id: userId } },
-            packages: { create: packages },
-        } as Prisma.OrderCreateInput);
-        return order;
-    }
+  async createOrder(
+    createOrderDto: CreateOrderDto,
+    userId: string,
+  ): Promise<Order | null> {
+    const { packages, ...orderData } = createOrderDto;
+    const order = await this.ordersRepository.createOrder({
+      ...orderData,
+      programedDate: new Date(orderData.programedDate),
+      user: { connect: { id: userId } },
+      packages: { create: packages },
+    } as Prisma.OrderCreateInput);
+    return order;
+  }
 
-    async getOrdersByUserId(userId: string): Promise<Order[] | null> {
-        return this.ordersRepository.getOrdersByUserId(userId);
+  async getOrdersByUserId(userId: string): Promise<Order[] | null> {
+    const orders = await this.ordersRepository.getOrdersByUserId(userId);
+    if (!orders) {
+      throw new NotFoundException('User does not has orders yet');
     }
+    return orders;
+  }
 
-    async getOrderById(id: string): Promise<Order | null> {
-        return this.ordersRepository.getOrderById(id);
+  async getOrderById(id: string): Promise<Order | null> {
+    const order = this.ordersRepository.getOrderById(id);
+    if (!order) {
+      throw new NotFoundException('Order not found');
     }
+    return order;
+  }
 }
