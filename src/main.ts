7f33075963a7c0e6dd6187bbe046c0f0fe3a11e5
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-09 04:11:57 -0600

    Revert ":wrench: Update CORS configuration to allow requests from production frontend"
    This reverts commit b46654f3ccf2bd8dc62d8cf550471c0d2f02d5c3.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000',
-      'https://boxful-technical-test-frontend.vercel.app/'
-    ],
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
