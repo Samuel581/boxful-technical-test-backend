105ad79120018a76c71b992729a06dedfbca4955
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-09 17:47:19 -0600

    :fire: Remove users controller and tests
    The users controller and its associated tests have been removed as they
    are no longer needed.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { UserRepository } from './users.repository';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   providers: [UsersService, UserRepository, PrismaService],
-  controllers: [UsersController],
+  controllers: [],
   exports: [UsersService],
 })
 export class UsersModule {}
