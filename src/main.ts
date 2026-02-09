7571bce6006ceed697b727cdbeac310c6482b3ad
Samuel Ortiz <samuel.adonay2001@gmail.com>
2026-02-09 14:20:41 -0600

    :safety_vest: Implement and config ValidationPipe globally

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000',
-      'https://boxful-technical-test-frontend.vercel.app/'
+    origin: [
+      'http://localhost:3000',
+      'https://boxful-technical-test-frontend.vercel.app/',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
